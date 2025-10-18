436f9d4dedfc9342bfbe9953ccb72fa5a543ed1b
codeKarma1 <eawoleke@gmail.com>
2025-10-18 22:47:29 +0100

    Add HNG Stage 0 profile card

---
+++ script.js
@@ -0,0 +1,11 @@
+const timeElement = document.querySelector('[data-testid="test-user-time"]');
+
+function updateTime() {
+  const currentTime = Date.now();
+
+  timeElement.textContent = currentTime;
+}
+immediately;
+updateTime();
+
+setInterval(updateTime, 500);
